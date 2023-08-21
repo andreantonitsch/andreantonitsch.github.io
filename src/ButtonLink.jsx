e472a8ffe8066b3258a2034ee14a20fbbfa779e0
André Antonitsch <andre.antonitsch@acad.pucrs.br>
2023-08-22 00:54:39 +0200

    v1

--- src/ButtonLink.jsx
+++ src/ButtonLink.jsx
@@ -1,13 +1,39 @@
+import { Button } from "@mui/material"
+import { styled } from '@mui/material/styles';
 
 function ButtonLink(props) {
 
     return <>
-        <a className="button" href={props.link}>
-            <span className="border"></span>
-            <span className="label">{props.text}</span>
-        </a>
+        <_LinkButton variant="outlined" href={props.link} className="button">{props.text}</_LinkButton>
     </>
 
 }
+
+const _LinkButton = styled(Button)(({ theme }) => ({
+    fontFamily: 'Oswald, sans-serif',
+    color:  "#ffffff",
+    backgroundColor: "#14121700",
+    textDecoration: "none",
+   
+    fontWeight: 200,
+    // fontWeight: 600,
+    fontSize: 20,
+    
+    borderRadius : 0,
+    borderWidth : 2,
+    borderColor:"#ffffff",
+
+    textJustify:"center",
+    alignItems: "center",
+
+    '&:hover': {
+      backgroundColor: "#ffffff",
+      color: "black",
+    //   color: "white",
+        borderRadius : 0,
+        borderWidth : 2,
+        borderColor:"#ffffff",
+    },
+  }));
 
 export default ButtonLink
